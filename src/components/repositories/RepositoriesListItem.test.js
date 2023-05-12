0bfe3fa70852856870d21eeecaa8630b2d4dbdb2
Pavel Smolov <plexsmol@gmail.com>
2023-05-12 19:15:56 -0400

    feat: implementing the feature - markgithubicon

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -16,7 +16,7 @@ function renderComponent() {
     description: 'A js library',
     owner: 'facebook',
     name: 'react',
-    html_url: '/repositories/facebook/react'
+    html_url: 'https://github.com/facebook/react'
   }
   render(
     <MemoryRouter>
@@ -31,6 +31,8 @@ test('shows a link to the github homepage for this repository', async () => {
   const { repository } = renderComponent();
 
   await screen.findByRole('img', { name: 'Javascript' });
-  const link = screen.getByRole('link');
+  const link = screen.getByRole('link', {
+    name: /github repository/i,
+  });
   expect(link).toHaveAttribute('href', repository.html_url);
 });
